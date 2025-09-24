0c154a3830ddc0ddb3447327ddbe9901d41c7b01
james junior <jamescookjunior@gmail.com>
2025-09-24 17:28:06 -0300

    refactor: refactoring and code and folders

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module task-manager-bff
 
 go 1.25.1
 
-require github.com/go-chi/chi/v5 v5.2.3
+require (
+	github.com/go-chi/chi/v5 v5.2.3
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -3,58 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
-	"io"
-	"os"
+
+	"task-manager-bff/handlers" 
+
 	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
-func getProjetosHandler(w http.ResponseWriter, r *http.Request) {
-	djangoAPI := os.Getenv("DJANGO_API")
-	apiToken := os.Getenv("API_TOKEN")
-
-	if djangoAPI == "" || apiToken == "" {
-		log.Fatal("Error: Variaveis de ambiente DJANGO_API e API_TOKEN não configuradas")
-		http.Error(w, "Erro de configuração interna do servidor", http.StatusInternalServerError)
-		return
-	}
-
-	req, err := http.NewRequest("GET", djangoAPI, nil)
-	if err != nil {
-		http.Error(w, "Erro ao criar requisição para a API interna", http.StatusInternalServerError)
-		log.Println("Erro NewRequest:", err)
-		return
-	}
-
-	req.Header.Add("Authorization", "Bearer " + apiToken)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		http.Error(w, "Erro ao fazer requisição para a API interna", http.StatusInternalServerError)
-		log.Println("Erro client.Do:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		http.Error(w, "Erro ao ler a resposta da API interna", http.StatusInternalServerError)
-		log.Println("Erro io.ReadAll:", err)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(resp.StatusCode)
-	w.Write(body)
-}
-
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Aviso: Não foi possível encontrar o arquivo .env.")
+	}
+
 	router := chi.NewRouter()
-	router.Get("/api/projetos", getProjetosHandler)
+
+	router.Get("/api/projetos", handlers.GetProjetosHandler)
 
 	port := ":8080"
 	log.Printf("Servidor BFF iniciado na porta %s", port)
 	log.Fatal(http.ListenAndServe(port, router))
 }
-
-
